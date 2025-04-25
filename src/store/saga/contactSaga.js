b02b62572579e3f07e3d53db6269d570a0868689
Bhavesh94V <bhaveshvishwakarma223@gmail.com>
2025-04-25 09:52:30 +0530

    first commit

---
+++ src/store/saga/contactSaga.js
@@ -0,0 +1,32 @@
+import { takeLatest, call, put } from 'redux-saga/effects';
+import axios from 'axios';
+import {
+    SUBMIT_CONTACT_FORM_REQUEST,
+    submitContactFormSuccess,
+    submitContactFormFailure
+} from '../action/contactActions';
+
+// API call function
+const submitContactFormAPI = (formData) => {
+    return axios.post('https://seu-website.onrender.com/api/contact', formData, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+};
+
+// Worker saga
+function* submitContactFormWorker(action) {
+    try {
+        const response = yield call(submitContactFormAPI, action.payload);
+        yield put(submitContactFormSuccess(response.data));
+    } catch (error) {
+        const errorMessage = error.response?.data?.message || 'Failed to send message';
+        yield put(submitContactFormFailure(errorMessage));
+    }
+}
+
+// Watcher saga
+export function* contactWatcherSaga() {
+    yield takeLatest(SUBMIT_CONTACT_FORM_REQUEST, submitContactFormWorker);
+}
