dde4a0bda82c7ab8c982c1117d157b171c31ad47
Bhavesh <159773209+Bhavesh94V@users.noreply.github.com>
2025-04-25 10:35:38 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,7 @@ import helmet from "helmet"
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
+app.set('trust proxy', 1);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
